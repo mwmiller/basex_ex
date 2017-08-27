3a2c7bd170927091e7a2ff56ede27a23a00b2787
Matt Miller <mwm@fastmail.com>
2017-08-27 14:14:09 +0200

    Include credo and apply some suggestions

--- lib/basex/modulemaker.ex
+++ lib/basex/modulemaker.ex
@@ -1,6 +1,6 @@
 defmodule BaseX.ModuleMaker do
   @moduledoc false
-  defmacro gen_module(name,abc,a,b,c,cba,vb,vc,vn) do
+  defmacro gen_module(name, abc, a, b, c, cba, vb, vc, vn) do
    quote bind_quoted: binding()  do
         defmodule name do
           @moduledoc false
@@ -12,7 +12,7 @@ defmodule BaseX.ModuleMaker do
             end
           end
 
-          defp decode_error(c), do: raise("Invalid input block: "<>c)
+          defp decode_error(c), do: raise("Invalid input block: " <> c)
 
           def  encode(binary), do: encode(binary, [])
           defp encode(<<>>, acc), do:  acc |> Enum.reverse |> Enum.join
@@ -27,22 +27,22 @@ defmodule BaseX.ModuleMaker do
           defp decode(<<this::binary-size(unquote(c)), rest::binary>>, acc), do: decode(rest, [gather_chars(this) | acc])
           defp decode(final, acc), do: decode(<<>>, [gather_chars(final) | acc])
 
-          defp char_val(c), do: Map.fetch!(unquote(cba),c)
+          defp char_val(c), do: Map.fetch!(unquote(cba), c)
           defp gather_chars(all), do: chars_to(all, bits_for_chars(all), 0)
 
           defp chars_to(<<c::binary-size(1)>>, final_size, acc) do
             final_acc = acc + char_val(c)
             max = Map.fetch!(unquote(vn), final_size)
-            cond do
-                  final_acc > max -> decode_error("value too large")
-                  true            -> final_acc |> :binary.encode_unsigned(:big)
-                 end
-              |> pad_chars(final_size)
+            if final_acc > max do
+              decode_error("value too large")
+            else
+              final_acc |> :binary.encode_unsigned(:big) |> pad_chars(final_size)
+             end
           end
-          defp chars_to(<<c::binary-size(1),rest::binary>>, final_size, acc), do: chars_to(rest, final_size, (acc+char_val(c)) * unquote(a))
+          defp chars_to(<<c::binary-size(1), rest::binary>>, final_size, acc), do: chars_to(rest, final_size, (acc + char_val(c)) * unquote(a))
 
           defp pad_chars(b, final_size) when bit_size(b) == final_size, do: b
-          defp pad_chars(b, final_size) when bit_size(b) <  final_size, do: pad_chars(<<0>><>b, final_size)
+          defp pad_chars(b, final_size) when bit_size(b) <  final_size, do: pad_chars(<<0>> <> b, final_size)
 
         end
     end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BaseX.Mixfile do
 
   def project do
     [app: :basex,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.4",
      name: "BaseX",
      source_url: "https://github.com/mwmiller/basex_ex",
@@ -29,6 +29,7 @@ defmodule BaseX.Mixfile do
     [
       {:earmark, "~> 1.1", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
+      {:credo, "~> 0.7", only: [:dev, :test]},
     ]
   end
 
